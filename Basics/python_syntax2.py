7e1d45a389af83ddb08b6551f08e06dfb64fe972
Christian Gracia <christiangracia.student@careerdevs.com>
2019-04-29 00:41:00 -0400

    further syntax learning, last commit of the night

---
+++ Basics/python_syntax2.py
@@ -0,0 +1,7 @@
+#using escape \ so the ' doesnt confuse Python
+escape_char = "This isn\'t flying, this is falling with style!"
+#accessing letters in a string with [i] index
+fifth_letter = "MONTY"[4]
+#using len() to find the length of a string
+parrot = "Norwegian Blue"
+print len(parrot)

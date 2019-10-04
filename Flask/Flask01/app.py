5437ed927abc7f6adfb862986378cd17363b97f9
Christian Gracia <christiangracia.student@careerdevs.com>
2019-10-03 23:06:07 -0400

    made flaks app render user names dynamically by adding a name param to request url

--- Flask/Flask01/app.py
+++ Flask/Flask01/app.py
@@ -6,4 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "lil_cuffy"
+    # second param in request is default value if name is not entered
+    name = request.args.get("name", "lil_cuffy")
+    return render_template("index.html", name=name)
+
+# set name value in request
+# ex 127.0.0.1:5000/?name=christian
